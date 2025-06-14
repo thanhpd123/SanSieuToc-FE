0736157c088dda4832ce3d821c905cd9cbea36c8
thanhpd123 <thanhpdhe173100@fpt.edu.vn>
2025-06-14 18:12:33 +0700

    fe1

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import Home from './component/Home';
 import Bongda from "./component/Bongda";
 import Bongro from "./component/Bongro";
 import Tennis from "./component/Tennis";
-import Login from "./component/Login";  
-import Booking from"./component/Booking"
+import Login from "./component/Login";
+import Booking from "./component/Booking"
 import BookingHistory from './component/BookingHistory';
 import Coaches from './component/Coaches';
-import CoachBooking from'./component/CoachBooking';
+import CoachBooking from './component/CoachBooking';
 import CoachBookingHistory from './component/CoachBookingHistory';
 import OwnerFields from './component/OwnerFields';
 import CreateField from './component/CreateField';
@@ -27,29 +27,29 @@ function App() {
     if (storedUser) setUser(storedUser);
   }, []);
 
-  
+
   return (
     <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path="/loaisan/6836d3231f7f6d0deb0f98d0" element={<Bongda />} />
-        <Route path="/loaisan/6836d3231f7f6d0deb0f98d1" element={<Bongro />} />
-        <Route path="/loaisan/6836d3231f7f6d0deb0f98d2" element={<Tennis />} />
-        <Route path="/register" element={<Login />} />
-        <Route path="/login" element={<Login />} />
+      <Route path='/home' element={<Home />} />
+      <Route path="/loaisan/6836d3231f7f6d0deb0f98d0" element={<Bongda />} />
+      <Route path="/loaisan/6836d3231f7f6d0deb0f98d1" element={<Bongro />} />
+      <Route path="/loaisan/6836d3231f7f6d0deb0f98d2" element={<Tennis />} />
+      <Route path="/register" element={<Login />} />
+      <Route path="/login" element={<Login />} />
 
-        <Route path="/manager/fields" element={<OwnerFields user={user} />} />
-        <Route path="/manager/fields/create" element={<CreateField user={user} />} />
-        <Route path="/manager/fields/:id/edit" element={<EditField user={user} />} />
-        <Route path="/manager/bookings" element={<OwnerSchedule user={user} />} />
-        <Route path="/manager/revenue" element={<RevenueReport user={user} />} />
-        <Route path="/manager/compare" element={<CompareRevenue user={user} />} />
+      <Route path="/manager/fields" element={<OwnerFields user={user} />} />
+      <Route path="/manager/fields/create" element={<CreateField user={user} />} />
+      <Route path="/manager/fields/:id/edit" element={<EditField user={user} />} />
+      <Route path="/manager/bookings" element={<OwnerSchedule user={user} />} />
+      <Route path="/manager/revenue" element={<RevenueReport user={user} />} />
+      <Route path="/manager/compare" element={<CompareRevenue user={user} />} />
 
 
-        <Route path="/booking/:id" element={<Booking />} />
-        <Route path="/lichsu-datsan" element={<BookingHistory />} />
-        <Route path="/coach" element={<Coaches />} />
-        <Route path="/coach/:coachId/booking" element={<CoachBooking />} />
-        <Route path="/coachbookinghistory" element={<CoachBookingHistory />} />
+      <Route path="/booking/:id" element={<Booking />} />
+      <Route path="/lichsu-datsan" element={<BookingHistory />} />
+      <Route path="/coach" element={<Coaches />} />
+      <Route path="/coach/:coachId/booking" element={<CoachBooking />} />
+      <Route path="/coachbookinghistory" element={<CoachBookingHistory />} />
 
     </Routes>
   );
